935f065e9b4fb7af9083f5a3b319e13fc902ad1e
TalitaDevsPuc <128756637+TalitaDevsPuc@users.noreply.github.com>
2023-06-21 18:49:43 -0300

    fullProject

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,4 @@
-window.onload = function() {
-    var elementBody = document.querySelector('body');
-    var elementBtnIncreaseFont = document.getElementById('increase-font');
-    var elementBtnDecreaseFont = document.getElementById('decrease-font');
-    var fontSize = 100;
-    var increaseDecrease = 22;
-
-    elementBtnIncreaseFont.addEventListener('click', function(event) {
-        fontSize = fontSize + increaseDecrease;
-        elementBody.style.fontSize = fontSize + '%';
-    });
-    
-    elementBtnDecreaseFont.addEventListener('click', function(event) {
-        fontSize = fontSize - increaseDecrease;
-        elementBody.style.fontSize = fontSize + '%';
-    });
-}
+document.addEventListener("DOMContentLoaded", function() {
+    var titulo = document.getElementById("titulo");
+    titulo.classList.add("mostrar");
+  });
